240e7435d7c0d6ef2144d6b8582a1a19f9474bed
wilbur61 <wroales@perscholas.org>
2024-02-25 19:56:34 -0500

    Initial commit  1.0

---
+++ src/test/java/com/example/sbactuator/SbactuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbactuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
